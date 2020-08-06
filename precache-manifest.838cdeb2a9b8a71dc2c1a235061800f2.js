afb23ef2463750b50bb0dbe70d40ab1e7c9a01ac
95Rajitha <95rajithalbandara@gmail.com>
2020-08-06 14:05:56 +0530

    Updates

---
+++ precache-manifest.838cdeb2a9b8a71dc2c1a235061800f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488c79c9af99d54381ba2378a73f7508",
+    "url": "/95Rajitha/contactmanagerApp/index.html"
+  },
+  {
+    "revision": "f60441570ed927234899",
+    "url": "/95Rajitha/contactmanagerApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8fe1c2c30052ed738928",
+    "url": "/95Rajitha/contactmanagerApp/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f60441570ed927234899",
+    "url": "/95Rajitha/contactmanagerApp/static/js/2.30b603b1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/95Rajitha/contactmanagerApp/static/js/2.30b603b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe1c2c30052ed738928",
+    "url": "/95Rajitha/contactmanagerApp/static/js/main.a922ce6f.chunk.js"
+  },
+  {
+    "revision": "d010a5fafec2cea5dbd2",
+    "url": "/95Rajitha/contactmanagerApp/static/js/runtime-main.4512df26.js"
+  }
+]);
